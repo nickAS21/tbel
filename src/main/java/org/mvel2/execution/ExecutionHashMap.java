ee67bb249e092ad8e501672c949df785f36a7abc
nick <nick@avalr.com.ua>
2025-02-26 18:36:28 +0200

    tbel: add unModifiable tests

--- src/main/java/org/mvel2/execution/ExecutionHashMap.java
+++ src/main/java/org/mvel2/execution/ExecutionHashMap.java
@@ -6,8 +6,6 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
